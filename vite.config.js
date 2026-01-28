8f52c5d3348361b40c486747d073540e5410efd3
turaabhaider <turaab.haider0110@gmail.com>
2026-01-28 09:30:30 -0800

    origin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // The new v4 way
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Add this here
-  ],
-  base: '/portfolio-2026/', // Essential for GitHub Pages
+  plugins: [react()],
+  base: '/portfolio-2026/', 
 })
